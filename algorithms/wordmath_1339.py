cacf73b8992c5cf6ce3a217dcdb5ec5100662098
janghb <janghb3730@gmail.com>
2024-09-14 22:11:21 +0900

    1

--- algorithms/wordmath_1339.py
+++ algorithms/wordmath_1339.py
@@ -1,10 +1,31 @@
 n = int(input())
 words = []
-d = {}
+d = [[] for _ in range(9)]
+alphabet = []
+made_num = []
 
 for _ in range(n):
     words.append(input())
 
-for str in words:
-    for s in str:
-        if s not in d:
+
+for digit in words:
+    for i in range(len(digit)):
+        d[len(digit) - i].append(digit[i])
+        if digit[i] not in alphabet:
+            alphabet.append(digit[i])
+
+for alb in alphabet:
+    num = 0
+    for i in range(9):
+        num += int(d[i].count(alb) * (10 ** (i - 1)))
+
+    made_num.append(num)
+
+made_num.sort(reverse = True)
+
+result = 0
+
+for i in range(len(made_num)):
+    result += made_num[i] * (9 - i)
+
+print(result)
