8c6607cf833903b8a2f7a156adc8bdff7223b119
Jang HyunBin <janghb3730@gmail.com>
2024-09-23 19:04:35 +0900

    1

--- algorithms/StackSequence_1874.py
+++ algorithms/StackSequence_1874.py
@@ -3,31 +3,45 @@ n = int(input())
 stack = []
 seq = []
 result = []
+available = True
 
 for _ in range(n):
     seq.append(int(input()))
 
-j = 0           # 현재 처리중인 seq index
+j = 1           # stack에 넣을 다음 값
 
-def nextlarger(i):
-    for k in range(j, n):
-        if seq[k] >= i:
-            print(seq[k])
-            return seq[k]
+# 수열 첫번째 값 처리
+for i in range(1, seq[0] + 1):
+    stack.append(i)
+    result.append('+')
+    j += 1
 
-i = 1
-find = True
+stack.pop()
+result.append('-')
 
-while j != n and find == True:
-    l = nextlarger(i)
-    while i != l:
-        stack.append(i)
-        result.append('+')
-        i += 1
-    while stack:
-        if stack.pop() == seq[j]:
-            result.append('-')
-            j += 1
+# 수열 나머지 값 처리
+for i in range(1, n):
+    if seq[i] < seq[i - 1]:
+        if seq[i] not in stack:
+            available = False
             break
+        k = 0
+        while k != seq[i]:
+            k = stack.pop()
+            result.append('-')
+    else:
+        if seq[i] < j:
+            available = False
+            break
+        for k in range(j, seq[i] + 1):
+            stack.append(k)
+            result.append('+')
+            j += 1
+        stack.pop()
+        result.append('-')
 
-print(result)
+if not available:
+    print("NO")
+else:
+    for r in result:
+        print(r)
