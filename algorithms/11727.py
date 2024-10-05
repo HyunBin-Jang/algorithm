03a3df240e58f4b37e7bc7402c1fc4145aa4f09e
Jang HyunBin <janghb3730@gmail.com>
2024-10-05 11:18:34 +0900

    1

--- algorithms/11727.py
+++ algorithms/11727.py
@@ -6,10 +6,10 @@ d[1] = 1
 if n == 1:
     print(d[1])
 elif n == 2:
-    d[2] = 2
+    d[2] = 3
     print(d[2])
 elif n >= 3:
-    d[2] = 2
+    d[2] = 3
     for i in range(3, n+1):
-        d[i] = d[i-1] + d[i-2]
+        d[i] = d[i-1] + d[i-2] * 2
     print(d[n] % 10007)
